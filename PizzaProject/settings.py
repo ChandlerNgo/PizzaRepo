22dacffeb2f19a9e000eed5c364acbcdf8618f71
ChandlerNgo <chandlerngo00@gmail.com>
2022-05-22 01:13:59 -0500

    set up finished

--- PizzaProject/settings.py
+++ PizzaProject/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'PizzaApp.apps.PizzaappConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
