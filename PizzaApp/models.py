61123e1b3232dce8efb851ee1c583aee0ddc5041
ChandlerNgo <chandlerngo00@gmail.com>
2022-05-25 17:54:53 -0500

    models creates

--- PizzaApp/models.py
+++ PizzaApp/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+User._meta.get_field("email")._unique = True
+User._meta.get_field("username")._unique = True
+
+class UserData(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    birthday = models.DateField()
+    rewardspoints = models.IntegerField()
+
+class Orders(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    orderdate = models.DateField()
+    ordertotal = models.DecimalField()
+    isDeliver = models.BooleanField()
+    isCash = models.BooleanField()
+    itemsordered = models.JSONField()
