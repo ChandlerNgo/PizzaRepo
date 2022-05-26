7ae05d849db31c76a29c494d6d352c1366c0c013
ChandlerNgo <chandlerngo00@gmail.com>
2022-05-26 14:54:10 -0500

    homepage html done

--- PizzaApp/urls.py
+++ PizzaApp/urls.py
@@ -4,4 +4,8 @@ from django.urls import path
 urlpatterns = [
     path("", views.index, name="index"),
     path("index", views.index, name="index"),
+    path("login", views.login, name="login"),
+    path("menu", views.menu, name="menu"),
+    path("order", views.order, name="order"),
+    path("rewards", views.rewards, name="rewards"),
 ]
--- PizzaApp/views.py
+++ PizzaApp/views.py
@@ -3,4 +3,25 @@ from django.shortcuts import render
 # Create your views here.
 
 def index(request):
-    return render(request,"index.html")
+    if request.user.is_authenticated:
+        response = {
+            "loginstate":"Log Out",
+        }
+        return render(request,"index.html",response)
+    else:
+        response = {
+            "loginstate":"Log In",
+        }
+        return render(request,"index.html",response)
+
+def login(request):
+    return render(request,"login.html")
+    
+def menu(request):
+    return render(request,"menu.html")
+
+def order(request):
+    return render(request,"order.html")
+
+def rewards(request):
+    return render(request,"rewards.html")
