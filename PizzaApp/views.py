78411b047ccf8a5a598059a6bf2512237d9c18d8
ChandlerNgo <chandlerngo00@gmail.com>
2022-05-26 16:19:31 -0500

    login + newfiles initialized

--- PizzaApp/views.py
+++ PizzaApp/views.py
@@ -15,6 +15,16 @@ def index(request):
         return render(request,"index.html",response)
 
 def login(request):
+    if request.user.is_authenticated:
+        response = {
+            "loginstate":"Log Out",
+        }
+        return render(request,"login.html",response)
+    else:
+        response = {
+            "loginstate":"Log In",
+        }
+        return render(request,"login.html",response)
     return render(request,"login.html")
     
 def menu(request):
